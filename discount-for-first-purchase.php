ce45575eb5ceca93c45e3b8dbfa09796c20c20fd
Merchans <addmarkovic@gmail.com>
2022-02-15 23:27:04 +0100

    add registration discount

--- discount-for-first-purchase.php
+++ discount-for-first-purchase.php
@@ -38,14 +38,7 @@ function generate_custom_coupon( $order ) {
 	$results = $wpdb->get_results( $query );
 	if ( !empty( $results ) && count( $results ) == 1 ) {
 
-	   	$code = '';
-        $keys = array_merge( range( 0, 9 ), range( 'a', 'z' ) );
-
-        for ($i = 0; $i < 10; $i++) {
-            $code .= $keys[array_rand( $keys )];
-        }
-
-		$code = strtoupper( $code );
+	   	$code = make_coupon_code();
 
 		$coupon_args = array(
 			'post_title' 	=> $code,
@@ -69,6 +62,58 @@ function generate_custom_coupon( $order ) {
 
 }
 
+function make_coupon_code() {
+
+	$code = '';
+	$keys = array_merge( range( 0, 9 ), range( 'a', 'z' ) );
+
+	for ($i = 0; $i < 10; $i++) {
+		$code .= $keys[array_rand( $keys )];
+	}
+
+	return strtoupper( $code );
+
+}
+
+add_action( 'user_register', 'add_discount_for_registration', 1 );
+function add_discount_for_registration($user_id) {
+
+	$user = get_user_by('id',$user_id);//Get the current user object
+
+	$coupon_title =  strtoupper( get_bloginfo('name')) . '10';
+	$coupon = new WC_Coupon( $coupon_title );//Get the coupon object
+	$emails = $coupon->get_email_restrictions();//Returns an empty array or array of emails
+	$emails[] = strtolower($user->user_email);//Add user's billing email address to the array
+	$emails = array_filter($emails);//Remove empty values
+	array_unique($emails);//Remove any duplicate values
+	$coupon->set_email_restrictions($emails);//Set the coupon's array with the updated array
+	$coupon->set_discount_type('percent');
+	$coupon->set_amount(10);
+	$coupon->set_usage_limit_per_user(1);
+	$coupon->set_individual_use('no');
+	$coupon->set_description('Slevový kód -10% za registraci na webu');
+	$coupon->save();//Save the coupon
+
+
+}
+
+/**
+ * @snippet       Add Text to Customer Processing Order Email
+ * @how-to        Get CustomizeWoo.com FREE
+ * @author        Rodolfo Melogli
+ * @testedwith    Woo 4.6
+ * @donate $9     https://businessbloomer.com/bloomer-armada/
+ */
+  
+add_action( 'woocommerce_email_before_order_table', 'bbloomer_add_content_specific_email', 20, 4 );
+  
+function bbloomer_add_content_specific_email( $order, $sent_to_admin, $plain_text, $email ) {
+   if ( $email->id == 'customer_new_account' ) {
+	echo '<h3>' . __( 'Děkujeme za Vaši registraci!', 'musilda' ) . '</h3>';
+	echo '<p>' . __( 'Jako poděkování jsme Vám vytvořili slevový kupón ve výši 10% na nákup.', 'musilda' ) . '<br/>';
+	echo __( 'Pro získání slevy, při nákupu zadete v pokladně následující kód:', 'musilda' ) . ' ' ,  strtoupper( get_bloginfo('name')) . '10' .'</p>';
+   }
+}
 /**
  * Display coupon code on the thank you page
  */
@@ -78,7 +123,7 @@ function display_generated_coupon( $order_id ) {
 
 	$coupon_code = get_post_meta( $order_id, 'generated_coupon', true );
 	if ( !empty( $coupon_code ) ) {
-		echo '<h3>' . __( 'Děkujme za váš první nákup!', 'musilda' ) . '</h3>';
+		echo '<h3>' . __( 'Děkujeme za váš první nákup!', 'musilda' ) . '</h3>';
 		echo '<p>' . __( 'Jako poděkování jsme Vám vytvořili slevový kupón ve výši 10% na další nákup.', 'musilda' ) . '<br/>';
 		echo __( 'Pro získání slevy, při dalším nákupu zadete v pokladně následující kód:', 'musilda' ) . ' ' , $coupon_code .'</p>';
 	}
